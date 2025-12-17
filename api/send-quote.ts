fed7f460dff124dec8b0bfa5d1f01bae8ff97160
BuffaloNYChris <60937504+buffalonychris@users.noreply.github.com>
2025-12-17 14:48:32 -0500

    Fix email API imports and result typing

--- api/send-quote.ts
+++ api/send-quote.ts
@@ -1,4 +1,4 @@
-import { handleEmailRequest } from './_email';
+import { handleEmailRequest } from './_email.js';
 
 export default async function handler(req: { method?: string; body?: unknown }, res: any) {
   return handleEmailRequest(req, res, 'Quote');
